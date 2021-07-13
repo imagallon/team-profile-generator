38da109b1d67e7e39f5d0500af6b0c77c834c670
Israel Magallon <browncans@gmail.com>
2021-07-12 21:20:21 -0700

    finished test for engineer and engineer.js

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,10 @@
 const Employee = require("../lib/Employee");
-const Manager = require('../lib/Manager.js');
+const Engineer = require("../lib/Engineer.js");
+const Manager = require("../lib/Manager.js");
+
 const employee = new Employee("Israel", 5, "israel@email.com");
 const manager = new Manager("Israel", 5, "israel@email.com", 12);
+const engineer = new Engineer("Israel", 5, "israel@email.com", "imagallon");
 
 describe("Employee", () => {
   describe("constructor", () => {
@@ -30,9 +33,9 @@ describe("Employee", () => {
   });
 
   describe("getRoles", () => {
-    const result = employee.getRole()
+    const result = employee.getRole();
     expect(result).toBe("Employee");
-  })
+  });
 });
 
 describe("Manager", () => {
@@ -44,31 +47,34 @@ describe("Manager", () => {
         email: "israel@email.com",
         officeNumber: 12,
       });
-    });    
+    });
   });
   describe("getRoles", () => {
-    const result = manager.getRole()
+    const result = manager.getRole();
     expect(result).toBe("Manager");
-  })
+  });
 });
 
 describe("Engineer", () => {
   describe("constructor", () => {
-    it("sets officeNumer and role for managers", () => {
-      expect(manager).toEqual({
+    it("sets gitHub and role for engineer", () => {
+      expect(engineer).toEqual({
         name: "Israel",
         id: 5,
         email: "israel@email.com",
-        officeNumber: 12,
+        gitHubUserName: "imagallon",
       });
-    });    
+    });
   });
   describe("getRoles", () => {
-    const result = manager.getRole()
-    expect(result).toBe("Manager");
-  })
-})
-
+    const result = engineer.getRole();
+    expect(result).toBe("Engineer");
+  });
+  describe("getGithub", () => {
+    const result = engineer.getGithub();
+    expect(result).toBe("imagallon");
+  });
+});
 
 // Properties:
 // -name: String - The employee's name. ("fred")
