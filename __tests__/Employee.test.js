13e96bef5a439751d24c68c5753c4c31cbdf5e0f
Israel Magallon <browncans@gmail.com>
2021-07-12 21:01:53 -0700

    finished manager.js and tests for managers

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,5 +1,7 @@
 const Employee = require("../lib/Employee");
+const Manager = require('../lib/Manager.js');
 const employee = new Employee("Israel", 5, "israel@email.com");
+const manager = new Manager("Israel", 5, "israel@email.com", 12);
 
 describe("Employee", () => {
   describe("constructor", () => {
@@ -32,6 +34,42 @@ describe("Employee", () => {
     expect(result).toBe("Employee");
   })
 });
+
+describe("Manager", () => {
+  describe("constructor", () => {
+    it("sets officeNumer and role for managers", () => {
+      expect(manager).toEqual({
+        name: "Israel",
+        id: 5,
+        email: "israel@email.com",
+        officeNumber: 12,
+      });
+    });    
+  });
+  describe("getRoles", () => {
+    const result = manager.getRole()
+    expect(result).toBe("Manager");
+  })
+});
+
+describe("Engineer", () => {
+  describe("constructor", () => {
+    it("sets officeNumer and role for managers", () => {
+      expect(manager).toEqual({
+        name: "Israel",
+        id: 5,
+        email: "israel@email.com",
+        officeNumber: 12,
+      });
+    });    
+  });
+  describe("getRoles", () => {
+    const result = manager.getRole()
+    expect(result).toBe("Manager");
+  })
+})
+
+
 // Properties:
 // -name: String - The employee's name. ("fred")
 // -id: Number - The unique id of the employee. (1)
